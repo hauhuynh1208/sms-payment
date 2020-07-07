ac7fe04991a694e8fdcd0637d48fb3c651af1ec2
vinhtu <vinhtu125@gmail.com>
2020-07-07 10:41:56 +0700

    conflig name function

--- src/Pages/Account/AccountPage.jsx
+++ src/Pages/Account/AccountPage.jsx
@@ -1,41 +1,6 @@
 import React from 'react';
-import {
-  Box,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Paper,
-} from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import MaterialTable from 'material-table';
-import { makeStyles } from '@material-ui/styles';
-
-const useStyles = makeStyles((theme) => ({
-  table_borderRight: {
-    borderWidth: 0,
-    borderRightWidth: 1,
-    borderBottomWidth: 1,
-    borderColor: '#f2f2f2',
-    borderStyle: 'solid',
-  },
-  table_borderBottom: {
-    borderWidth: 0,
-    borderBottomWidth: 1,
-    borderColor: '#f2f2f2',
-    borderStyle: 'solid',
-  },
-  style_header: {
-    backgroundColor: '#00a3a3',
-  },
-  text__header: {
-    color: 'white',
-  },
-  table_rowHeader: {
-    // backgroundColor: '#00a3a3',
-  },
-}));
 
 const headerStyle = {
   borderWidth: 1,
@@ -52,110 +17,45 @@ const cellStyle = {
 };
 
 const AccountPage = (props) => {
-  const { dataAccount, dataAccountMain } = props;
-
+  const { mainAccount, accounts } = props;
+  const columnMainAccount = [
+    { title: 'FirstName', field: 'firstname' },
+    { title: 'LastName', field: 'lastname' },
+    { title: 'Email', field: 'email' },
+    { title: 'Phone', field: 'phone' },
+    { title: 'Address', field: 'address' },
+  ];
   const columns = [
     { title: 'FirstName', field: 'firstname' },
     { title: 'LastName', field: 'lastname' },
     { title: 'Email', field: 'email' },
     { title: 'Phone', field: 'phone' },
     { title: 'Address', field: 'address' },
-    {
-      title: 'Mật Khẩu',
-      field: 'password',
-      editable: 'onAdd',
-    },
   ];
-
-  const classes = useStyles();
-
+  const arrMainAccount = [];
+  arrMainAccount.push(mainAccount);
   return (
     <Box p={5} width="100%">
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow className={classes.table_rowHeader}>
-              <TableCell align="left" style={{ fontSize: 20 }}>
-                Main Account
-              </TableCell>
-              <TableCell />
-              <TableCell />
-              <TableCell />
-              <TableCell />
-            </TableRow>
-            <TableRow className={classes.style_header}>
-              <TableCell
-                align="left"
-                className={[classes.table_borderRight, classes.text__header]}
-              >
-                FirstName
-              </TableCell>
-              <TableCell
-                align="left"
-                className={[classes.table_borderRight, classes.text__header]}
-              >
-                LastName
-              </TableCell>
-              <TableCell
-                align="left"
-                className={[classes.table_borderRight, classes.text__header]}
-              >
-                Email
-              </TableCell>
-              <TableCell
-                align="left"
-                className={[classes.table_borderRight, classes.text__header]}
-              >
-                Phone
-              </TableCell>
-              <TableCell
-                align="left"
-                className={[classes.table_borderBottom, classes.text__header]}
-              >
-                Address
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {dataAccountMain.map((row) => (
-              <TableRow>
-                <TableCell
-                  component="th"
-                  scope="row"
-                  className={classes.table_borderRight}
-                >
-                  {row.firstname}
-                </TableCell>
-                <TableCell align="left" className={classes.table_borderRight}>
-                  {row.lastname}
-                </TableCell>
-                <TableCell align="left" className={classes.table_borderRight}>
-                  {row.email}
-                </TableCell>
-                <TableCell align="left" className={classes.table_borderRight}>
-                  {row.phone}
-                </TableCell>
-                <TableCell align="left" className={classes.table_borderBottom}>
-                  {row.address}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <MaterialTable
+        title="Main Account"
+        columns={columnMainAccount}
+        data={arrMainAccount}
+        options={{
+          search: false,
+          paging: false,
+          headerStyle: headerStyle,
+          cellStyle: cellStyle,
+        }}
+      />
       <Box pt={5} />
       <MaterialTable
         title="Manager Account"
         columns={columns}
-        data={dataAccount}
+        data={accounts}
         options={{
-          headerStyle,
-          cellStyle,
-        }}
-        localization={{
-          toolbar: {
-            nRowsSelected: 10,
-          },
+          headerStyle: headerStyle,
+          cellStyle: cellStyle,
+          pageSize: 10,
         }}
         editable={{
           onRowAdd: (newData) =>
@@ -166,10 +66,9 @@ const AccountPage = (props) => {
                 email: newData.email,
                 phone: newData.phone,
                 address: newData.address,
-                password: newData.password,
               };
               setTimeout(() => {
-                props.postAccount(newDatas);
+                props.createAccount(newDatas);
                 resolve();
               }, 600);
             }),
@@ -177,7 +76,7 @@ const AccountPage = (props) => {
             new Promise((resolve) => {
               setTimeout(() => {
                 const id = newData._id;
-                props.putAccount(id, newData);
+                props.editAccount(id, newData);
                 resolve();
               }, 600);
             }),
